37ae4c783684e478104171036e62bf730bcad590
Nathan Stitt <nathan@stitt.org>
2024-10-21 11:09:51 -0500

    add an example Dockerfile for production build
    Will likely need tweaked for per-app settings, this is what management app is using

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+}
 
 export default withVanillaExtract(nextConfig)
